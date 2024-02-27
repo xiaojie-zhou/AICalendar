3f1f70cf48b9f080b6bfdc6fae4899a681dc4f53
Xiaojie Zhou <46155366+xiaojie-zhou@users.noreply.github.com>
2024-02-27 14:23:38 -0800

    test version
    TODO: OpenAI ChatBox
    TODO: Integrate with Todoist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <iframe
+              title="GCal"
+              id='GCal'
+              src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%23ffffff&showTitle=0&showPrint=0&mode=WEEK&src=dG9ueXpob3V6eGpAZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=MG9mcW0xMTZuYTc0djBuNWJicDNqMmpla2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZmFtaWx5MTEwNDg4NzIzMzIxMTgxMDQ2ODhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4uY2hpbmEjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=aHQzamxmYWFjNWxmZDYyNjN1bGZoNHRxbDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%234285F4&color=%2333B679&color=%230083c6&color=%2333ae06&color=%23a2845e&color=%23B39DDB"
+              width="800" height="800" frameBorder="0" scrolling="no" align='left'>
+          </iframe>
+          <table align='right'>
+              <tbody>
+                  <tr>
+                      <td>
+                          <iframe title='Todoist' src="https://todoist.com/app" loading='eager' width='400px' height='500px'
+                                  frameBorder='0'>
+                          </iframe>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          {/*<OPENAI/>*/}
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+
+
+      </div>
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+        <script>
+        $(document).ready(function () {
+            function refresh(){
+            var iframe = document.getElementById('GCal');
+            iframe.src = iframe.src;}}
+        </script>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
