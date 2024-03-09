3d560cfd9022801e5097c23f9767e9821085139b
Xiaojie Zhou <46155366+xiaojie-zhou@users.noreply.github.com>
2024-03-09 11:09:41 -0800

    Todo

--- src/todoistapi.js
+++ src/todoistapi.js
@@ -2,8 +2,8 @@ import {TodoistApi} from "@doist/todoist-api-typescript";
 // const TodoistApi = require('@doist/todoist-api-typescript');
 //
 // const token = process.env.REACT_APP_TODOIST;
-const todoist = new TodoistApi("9fb5ef8fd11a54d920f332ee2ed835ea7d9f5694");
-let project_id ='2203598416'
+const todoist = new TodoistApi(process.env.REACT_APP_TODOIST);
+let project_id =process.env.REACT_APP_TODOIST_Project
 
 export async function simpletask(name, time) {
     todoist.addTask({
